ed45c21e44a2b10f2ed946e62ac58b34ef56be7a
Ramin Afandiyev <raminefendi@gmail.com>
2021-04-18 15:17:52 +0400

    Mock data added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {Pressable, FlatList, StyleSheet, Text, View} from 'react-native';
+import data from '../constants/data.json';
 
 const Home = ({navigation}: any) => {
+  const renderItem = ({item}: any) => (
+    <View>
+      <Text>{item.id}</Text>
+    </View>
+  );
+
   return (
     <View>
       <Text>Home</Text>
+
+      <FlatList
+        horizontal
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+
       <Pressable onPress={() => navigation.navigate('Profile')}>
         <Text>Go to Profile</Text>
       </Pressable>
